ce08f31121615c4ace5d78baf5f44be801c1e0fd
Mojca <marin.mojca14@gmail.com>
2024-02-21 22:29:07 +0100

    Omogocen Cross origin

--- src/main/java/com/rentu/rentu/controllers/ReservationController.java
+++ src/main/java/com/rentu/rentu/controllers/ReservationController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
+@CrossOrigin
 public class ReservationController {
 
     @Autowired
--- src/main/java/com/rentu/rentu/controllers/UserController.java
+++ src/main/java/com/rentu/rentu/controllers/UserController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController {
 
     private final UserRepository userRepository;
